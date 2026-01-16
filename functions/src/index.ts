a409047861131c1af64abb87da14a92877a43d16
mclaramarinho <marinho.claramb@gmail.com>
2026-01-15 21:17:23 -0300

    first commit

---
+++ functions/src/index.ts
@@ -0,0 +1,62 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import {setGlobalOptions} from "firebase-functions";
+import {onRequest} from "firebase-functions/https";
+import * as logger from "firebase-functions/logger";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// For cost control, you can set the maximum number of containers that can be
+// running at the same time. This helps mitigate the impact of unexpected
+// traffic spikes by instead downgrading performance. This limit is a
+// per-function limit. You can override the limit for each function using the
+// `maxInstances` option in the function's options, e.g.
+// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
+// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
+// functions should each use functions.runWith({ maxInstances: 10 }) instead.
+// In the v1 API, each function can only serve one request per container, so
+// this will be the maximum concurrent request count.
+setGlobalOptions({ maxInstances: 10 });
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+
+// TODO - createUser
+export const createUser = onRequest((request, response) => {
+    logger.info("Create User request received", {structuredData: true});
+
+    // name / lastName / email / birthdate / gender / termsAccepted
+    response.status(501);
+    response.send("Create User - Not Implemented");
+};
+// TODO - updateUser
+// TODO - deleteUser
+// TODO - getUser
+
+// TODO - createPet
+// TODO - updatePet
+// TODO - deletePet
+// TODO - getPets
+
+// TODO - createProfessional
+// TODO - updateProfessional
+// TODO - deleteProfessional
+// TODO - getProfessionals
+
+// TODO - createVaccineSchedule
+// TODO - updateVaccineSchedule
+// TODO - getVaccineSchedules
+
+// TODO - recordVaccineAdministration
+// TODO - getVaccinationHistory
