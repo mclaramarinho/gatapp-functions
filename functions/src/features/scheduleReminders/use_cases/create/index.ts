d50dbe7fb9589b73f3d5397cf1982fef6b38eb53
mclaramarinho <marinho.claramb@gmail.com>
2026-01-27 13:54:36 -0300

    feat: enhance createScheduleReminder function with vaccination schedule validation

--- functions/src/features/scheduleReminders/use_cases/create/index.ts
+++ functions/src/features/scheduleReminders/use_cases/create/index.ts
@@ -6,31 +6,48 @@ import { CreateScheduleReminderModel }
   from "./models/CreateScheduleReminderModel";
 import { firestore } from "../../../../shared/firestore/init";
 import { FirestoreCollections } from "../../../../shared/firestore/collections";
+import { MissingQueryParam }
+  from "../../../../shared/exceptions/MissingQueryParam";
+import { getAuthTokenFromRequest }
+  from "../../../../shared/auth/getAuthTokenFromRequest";
+import { VaccinationScheduleValidations }
+  from "../../../../shared/validations/VaccinationScheduleValidations";
 
 /**
  * HTTP function to create a schedule reminder in Firestore.
  */
 export const createScheduleReminder = onRequest(async (req, res) => {
   try {
-    const authToken = req.headers.authorization?.split("Bearer ")[1];
-    const uid = await validateAuthToken(authToken);
+    const uid = await validateAuthToken(getAuthTokenFromRequest(req));
+    const model = new CreateScheduleReminderModel(req.body);
 
-    // TODO - validate schedule exists
-
-    // TODO - validate schedule pet belongs to user (uid)
-
-    const body = req.body;
-
-    const model = new CreateScheduleReminderModel(body);
-
+    const scheduleType = req.params.scheduleType as string;
+    if (!scheduleType) throw new MissingQueryParam("scheduleType");
+    switch (scheduleType) {
+      case "1":
+        // VACCINATION
+        // validate schedule exists
+        await VaccinationScheduleValidations
+            .existsAndBelongsToUser(model.scheduleId, uid);
+        break;
+      case "2":
+        // MEDICATION
+        // TODO - implement
+        res.status(501).send({ error: "Not implemented yet." });
+        return;
+      default:
+        res.status(400).send({ error: "Invalid scheduleType value" });
+    }
     const docId = await firestore
         .collection(FirestoreCollections.ScheduleReminders)
         .add({
           ...model,
         });
-    res.status(201).send({ id: docId.id });
+    res.status(201).send({ id: docId.id, ...model });
+    return;
   } catch (error) {
     const { statusCode, message } = ExceptionsHandler.handle(error as Error);
     res.status(statusCode).send({ error: message });
+    return;
   }
 });
